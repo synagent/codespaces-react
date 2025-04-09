70aea9e4a025524fa1044ac76c67a06b52a8167b
Synagnet <founder@syn-agent.com>
2025-04-09 03:53:35 +0000

    main

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,17 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+        Your AI Assistant
+for Business Efficiency <span className="heart">♥️</span> 
         </p>
         <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
+        Your AI Assistant
+        for Business Efficiency.
         </p>
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://youtube.com/@synagent?si=0DcaXGOLP91eL03E"
             target="_blank"
             rel="noopener noreferrer"
           >
