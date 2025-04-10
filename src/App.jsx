a0ad39d32022c667f56c11b6a4be46e386455c0e
Synagnet <founder@syn-agent.com>
2025-04-10 04:16:26 +0000

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import LandingPage from './components/LandingPage';
+import Hero from "./components/Hero";
+import VideoSection from "./components/VideoSection";
+import Animation from "./components/Animation";
+import Footer from "./components/Footer";
+import BackgroundCircuits from "./components/BackgroundCircuits";
 import './index.css';
 
 function App() {
-  return <LandingPage />;
+  return (
+    <div className="relative bg-radial text-white font-sans overflow-x-hidden">
+      <BackgroundCircuits />
+      <Hero />
+      <VideoSection />
+      <Animation />
+      <Footer />
+    </div>)
 }
 
 export default App;
